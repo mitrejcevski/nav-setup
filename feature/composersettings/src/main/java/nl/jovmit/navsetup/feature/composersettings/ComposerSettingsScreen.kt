7e483ae213d2144da2d5ea1b11b721396a104e52
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2025-03-09 12:04:31 +0100

    Introduce feature:composersettings module

--- feature/composersettings/src/main/java/nl/jovmit/navsetup/feature/composersettings/ComposerSettingsScreen.kt
+++ feature/composersettings/src/main/java/nl/jovmit/navsetup/feature/composersettings/ComposerSettingsScreen.kt
@@ -1,4 +1,4 @@
-package nl.jovmit.navsetup.composersettings
+package nl.jovmit.navsetup.feature.composersettings
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,7 +19,7 @@ import nl.jovmit.navsetup.shared.ui.theme.NavSetupTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ComposerSettingsScreen(
+internal fun ComposerSettingsScreen(
   onNavigateUp: () -> Unit
 ) {
   Scaffold(
