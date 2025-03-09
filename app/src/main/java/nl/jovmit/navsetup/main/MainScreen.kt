501052b994cb72a200ed24ca904cefff77eb0e56
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2025-03-09 17:21:36 +0100

    Wire up the main screen navigation (bottom nav bar)

--- app/src/main/java/nl/jovmit/navsetup/main/MainScreen.kt
+++ app/src/main/java/nl/jovmit/navsetup/main/MainScreen.kt
@@ -1,14 +1,28 @@
 package nl.jovmit.navsetup.main
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import nl.jovmit.navsetup.emails.EmailsListDestination
+import nl.jovmit.navsetup.emails.emailsListScreen
+import nl.jovmit.navsetup.emails.navigateToEmailsList
+import nl.jovmit.navsetup.profile.ProfileDestination
+import nl.jovmit.navsetup.profile.navigateToProfile
+import nl.jovmit.navsetup.profile.profileScreen
 import nl.jovmit.navsetup.ui.theme.NavSetupTheme
 
 @Composable
@@ -16,7 +30,28 @@ fun MainScreen(
   onOpenEmailDetails: (emailId: Int) -> Unit,
   onComposeNewEmail: () -> Unit
 ) {
-
+  val navController = rememberNavController()
+  Scaffold(
+    bottomBar = {
+      MainBottomBar(
+        hierarchy = navController.currentBackStackEntryAsState().value?.destination?.hierarchy,
+        onNavigateToEmails = { navController.navigateToEmailsList() },
+        onNavigateToProfile = { navController.navigateToProfile() }
+      )
+    }
+  ) { paddingValues ->
+    NavHost(
+      modifier = Modifier.padding(paddingValues),
+      navController = navController,
+      startDestination = EmailsListDestination
+    ) {
+      emailsListScreen(
+        onOpenEmailDetails = onOpenEmailDetails,
+        onComposeNewEmail = onComposeNewEmail
+      )
+      profileScreen()
+    }
+  }
 }
 
 @Composable
@@ -27,12 +62,12 @@ private fun MainBottomBar(
 ) {
   NavigationBar {
     NavigationBarItem(
-      selected = true,
+      selected = hierarchy?.any { it.hasRoute(EmailsListDestination::class) } == true,
       icon = { Icon(imageVector = Icons.Default.Email, "emails") },
       onClick = onNavigateToEmails
     )
     NavigationBarItem(
-      selected = false,
+      selected = hierarchy?.any { it.hasRoute(ProfileDestination::class) } == true,
       icon = { Icon(imageVector = Icons.Default.Person, "profile") },
       onClick = onNavigateToProfile
     )
