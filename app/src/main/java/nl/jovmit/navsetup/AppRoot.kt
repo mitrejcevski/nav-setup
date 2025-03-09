f0bb201b2b319b85c69134810caf1997e3e77914
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2025-03-09 17:03:26 +0100

    Introduce Login navigation

--- app/src/main/java/nl/jovmit/navsetup/AppRoot.kt
+++ app/src/main/java/nl/jovmit/navsetup/AppRoot.kt
@@ -3,14 +3,20 @@ package nl.jovmit.navsetup
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import nl.jovmit.navsetup.login.LoginDestination
+import nl.jovmit.navsetup.login.loginScreen
+import nl.jovmit.navsetup.signup.navigateToSignUp
 
 @Composable
 fun AppRoot() {
   val navController = rememberNavController()
   NavHost(
     navController = navController,
-    startDestination = ""
+    startDestination = LoginDestination
   ) {
-
+    loginScreen(
+      onNavigateToMain = {},
+      onNavigateToSignUp = { navController.navigateToSignUp() }
+    )
   }
 }
