a3dd21eec716e1b10785edef2f44b12cec66ccd8
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2025-03-10 09:07:10 +0100

    Introduce composer navigation

--- app/src/main/java/nl/jovmit/navsetup/composer/ComposerScreenRoot.kt
+++ app/src/main/java/nl/jovmit/navsetup/composer/ComposerScreenRoot.kt
@@ -2,13 +2,31 @@ package nl.jovmit.navsetup.composer
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.serialization.Serializable
 import nl.jovmit.navsetup.ui.theme.NavSetupTheme
+
+@Serializable
+private data object ComposerDestination
 
 @Composable
 fun ComposerScreenRoot(
   onNavigateUp: () -> Unit
 ) {
-
+  val navController = rememberNavController()
+  NavHost(
+    navController = navController,
+    startDestination = ComposerDestination
+  ) {
+    composable<ComposerDestination> {
+      ComposerScreen(
+        onNavigateUp = onNavigateUp,
+        onOpenSettings = {}
+      )
+    }
+  }
 }
 
 @Preview
